882d5ef51c78573cbbc3bc4bd947ca0035298049
Muhammad Raza Adzani <131844692+MuhammadRazaAdzanii@users.noreply.github.com>
2023-11-06 13:52:44 +0700

    Update test_db.sql

--- db/test_db.sql
+++ db/test_db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL PRIMARY KEY AUTO INCREAMENT,
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `email` varchar(255) NOT NULL UNIQUE,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL
